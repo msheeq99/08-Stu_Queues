36f6e76fce685242dbe9252cb23b303093bf4ed8
Mohamud <msheeq99@outlook.com>
2023-01-23 20:35:47 +0000

    addToQueue(el) {     return this.container.push

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // create the Queue class with two methods to remove and add an element
 class Queue {
-  
+    constructor(container = []) {
+        this.container = container;
+      }
+    
+      addToQueue(el) {
+        return this.container.add(el);
+      }
+        
 }
 
 module.exports = Queue;
+
+
+
+
+
+// Notes pop element removes an element from the top of the stack
+// Shift element removes an element from the front of the queue
